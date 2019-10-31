5e4d51c55199acaec55720f680492b132ad522f1
Vika <you@example.com>
2019-11-01 00:05:00 +0200

    1st commit

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person["name"]);
+
+let arr = ['plum.png','orange.png','apple.jpg'];
+
+console.log(arr[2]);
+
+// alert("hello world!");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = +prompt("Are you here?","Yes");
+
+// console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
