a5daf80cdaaa2d4063d48235cc7f2b57436de266
Vika <you@example.com>
2019-11-02 14:02:53 +0200

    2nd hometask

--- script.js
+++ script.js
@@ -18,10 +18,8 @@
 
 // console.log(answer);
 
-let money = +prompt("Ваш бюджет на месяц?","Yes");
-let time = +prompt("Введите дату в формате YYYY-MM-DD","YYYY-MM-DD");
-let qstn1 = prompt("Введите обязательную статью расходов в этом месяце","Yes");
-let qstn2 = +prompt("Во сколько обойдется?","YYYY-MM-DD");
+let money = +prompt("Ваш бюджет на месяц?",""),
+    time = +prompt("Введите дату в формате YYYY-MM-DD","");
 
 let appData = {
     budjet: money,
@@ -29,15 +27,61 @@ let appData = {
     optionalExpenses: {},
     savings: false,
     income: [],
-    expenses: {
-        qstn1: qstn1, 
-        qstn2: qstn2,
-    }
+    expenses: {},
+};
+
+// let i = 0;
+// while (i < 2) {
+//     let a = +prompt("Введите обязательную статью расходов в этом месяце",''),
+//         b = +prompt("Во сколько обойдется?",'');
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+//     && a != '' && b != '' && a.length < 50) {
+//     console.log("done");
+//     appData.expenses[a] = b;
+//      i++;
+//      } 
+// }
+
+// let i = 0;
+// do {
+//     let a = +prompt("Введите обязательную статью расходов в этом месяце",''),
+//         b = +prompt("Во сколько обойдется?",'');
+//     i++;
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+//     && a != '' && b != '' && a.length < 50) {
+//     console.log("done");
+//     appData.expenses[a] = b;
+//     i++;
+//     }  
+// }
+// while (i < 2);
+
+for (let i = 0; i < 2; i++) {
+    let a = +prompt("Введите обязательную статью расходов в этом месяце",''),
+        b = +prompt("Во сколько обойдется?",'');
+
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+        && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } 
 }
 
-alert ("Бюджет на 1 день:"+qstn2/30);
+appData.moneyPerDay = appData.budjet/30;
+alert("fdsfd:" + appData.moneyPerDay);
 
-console.log(appData)
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальый уровень достатка")
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Vysokiy");
+} else {
+    console.log("Oshibka");
+}
+// console.log(appData)
+
+// alert ("Бюджет на 1 день:"+qstn2/30);
 
 // console.log(typeof(answer));
 
@@ -50,3 +94,16 @@ console.log(appData)
 // console.log(incr++);
 // console.log(decr--);
 // console.log(5%2);
+
+// if (2*4 == 8) {
+//     console.log("verno")
+// } else {
+//     console.log("neverno")
+// }
+
+// for (let i = 1; i < 8; i++){
+//     if (i==6) {
+//         continue
+//     }
+//     console.log(i)
+// }
